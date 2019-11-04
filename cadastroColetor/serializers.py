45e8626a0f62fe6b4b8560b518723dfe01a3b83a
Lucas Munhoz <lubarlettamunhoz@gmail.com>
2019-11-04 14:14:15 -0300

    Adicionar senha

--- cadastroColetor/serializers.py
+++ cadastroColetor/serializers.py
@@ -5,4 +5,4 @@ from cadastroColetor.models import Coletor
 class ColetorSerializer(serializers.ModelSerializer):
     class Meta:
         model = Coletor
-        fields = ['id','nome','cpf','email']
+        fields = ['id','nome','cpf','email','senha']
